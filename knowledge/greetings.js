c49bfd22fc6380a026375cc15025656856cc1f7f
Qadeer Khan <txcowner@gmail.com>
2025-08-26 08:05:42 +0500

    Create greetings.js

---
+++ knowledge/greetings.js
@@ -0,0 +1,19 @@
+// Salam dua se mutalliq rules
+export const greetings = [
+    {
+        keywords: ['hello', 'hi', 'salam', 'hey', 'salamun alaikum', 'assalam-o-alaikum'],
+        response: "Walaikum Assalam! Jee, farmaye."
+    },
+    {
+        keywords: ['how are you', 'kya haal', 'kaise ho', 'kesy ho', 'haal chaal'],
+        response: "Alhamdulillah, main theek hoon. Shukriya. Aap sunayein?"
+    },
+    {
+        keywords: ['thanks', 'thank you', 'shukriya', 'mehrbani'],
+        response: "Koi baat nahi. Yeh mera farz hai."
+    },
+    {
+        keywords: ['bye', 'goodbye', 'allah hafiz', 'khuda hafiz'],
+        response: "Aap se baat karke acha laga. Allah Hafiz!"
+    }
+];
